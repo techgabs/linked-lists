197f01d53a21d0ba7e1eeb56e648f778410ef1de
techgabs <techgabs@gmail.com>
2018-09-30 19:24:52 +0530

    Aded insert to end method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,10 +2,22 @@ public class LinkedList<T> {
 
     private Node head;
 
-    private void insert(T data){
+    private void insertToBeginning(T data){
         Node<T> temp = new Node<T>(data);
         temp.setLink(head);
         head = temp;
+    }
+    private void insertToEnd(T data){
+
+        Node<T> newNode = new Node<T>(data);
+        if(head==null) { head = newNode;}
+        else {
+            Node temp = head;
+            while (temp.getLink() != null) {
+                temp = temp.getLink();
+            }
+            temp.setLink(newNode);
+        }
     }
     private void print(){
         Node<T> temp = head;
@@ -16,11 +28,11 @@ public class LinkedList<T> {
     }
     public static void main(String[] args){
         LinkedList list = new LinkedList();
-        list.insert(2);
-        list.insert(3);
-        list.insert(6);
-        list.insert(8);
-        list.insert(9);
+        list.insertToEnd(2);
+        list.insertToEnd(3);
+        list.insertToEnd(6);
+        list.insertToEnd(8);
+        list.insertToEnd(9);
 
         list.print();
 
